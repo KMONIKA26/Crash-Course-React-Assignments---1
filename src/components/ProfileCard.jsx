e39518dab5ded05733ff826e427e4ccfc81eb1be
KMONIKA26 <raomonika343@gmail.com>
2024-05-23 23:16:30 +0530

    react Done

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,18 @@
+// src/components/ProfileCard.jsx
+
+import React from "react";
+
+function ProfileCard({ name, age, location }) {
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>Age: {age}</p>
+      <p>Location: {location}</p>
+    </div>
+  );
+}
+
+export default ProfileCard;
+
+
+
